e14a41dbe3b2111aea743fa2430f8ff3f63c4548
ShitMintz <100096969+ShitMintz@users.noreply.github.com>
2022-07-06 09:58:37 +1200

    mhm

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -10,12 +10,12 @@ var velocity = Vector2.ZERO
 export (float) var friction = 10
 export (float) var acceleration = 25
 
-enum state {Attack, Fall, Pushing, Jump, Idle, Roll, Running}
+enum state {Attack, Fall, Pushing, Jump, StartJump, Idle, Roll, Running}
 
 onready var Player_state = state.Idle
 
 func update_animation(anim):
-	$AnimationPlayer.play(anim)
+	pass
 
 func handle_state(state):
 	pass
@@ -28,7 +28,25 @@ func get_input():
 		velocity.x = move_toward(velocity.x, 0, friction)
 
 func _physics_process(delta):
-	pass
+	get_input()
+	if velocity.x == Vector2.ZERO:
+		Player_state = state.Idle
+	elif Input.is_action_just_pressed("Jump") and is_on_floor():
+		Player_state = state.StartJump
+	elif velocity.x != 0:
+		Player_state = state.Running
+	
+	if not is_on_floor():
+		if velocity.y < 0:
+			Player_state = state.jump
+		if velocity.y > 0:
+			Player_state = state.Fall
+
+	handle_state(Player_state)
+	update_animation(Player_state)
+	#set gravity
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
 
 func _ready():
 	$AnimationPlayer.play("Idle")
